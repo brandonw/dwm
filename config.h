fcbe5a166241c644e3281fd10fac0a8788faa4cf
Brandon Waskiewicz <brandon.waskiewicz@gmail.com>
2014-10-01 20:19:04 -0400

    Add slimlock wm command

--- config.h
+++ config.h
@@ -67,12 +67,14 @@ static const char terminal[]  = "urxvt";
 static const char *termcmd[]  = { terminal, NULL };
 static const char scratchpadname[]  = "scratchpad";
 static const char *scratchpadcmd[]  = { terminal, "-name", scratchpadname, "-geometry", "150x40", NULL };
+static const char *lockcmd[]        = { "slimlock" };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_x,      togglescratch,  {.v = scratchpadcmd } },
+	{ MODKEY,                       XK_s,      spawn,          {.v = lockcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
