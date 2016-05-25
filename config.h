76d298c8b49fa76026836d41fa572f8cc0f431c9
Brandon Waskiewicz <brandon.waskiewicz@analytehealth.com>
2016-05-24 20:07:00 -0400

    Add chromium wm class rule

--- config.h
+++ config.h
@@ -34,7 +34,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Firefox",  NULL,       NULL,       1 << 8,       False,       -1 }
+	{ "Firefox",  NULL,       NULL,       1 << 8,       False,       -1 },
+	{ "chromium",  NULL,       NULL,       1 << 8,       False,       -1 }
 };
 
 /* layout(s) */
